ebffe0d434f311555d9929631eaa889ddefd0d6f
Adam King <adamjking3@gmail.com>
2026-03-18 18:32:00 -1000

    Migrate published site template to shared MDXRenderer (el-3ix1)

--- packages/create-inkloom/templates/default/dist/assets/channel-B4fdroZG.js
+++
@@ -1,1 +0,0 @@
-import{U as a,D as n}from"./mermaid.core-c6Vc8FXi.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- packages/create-inkloom/templates/default/dist/assets/clone-hWzKUA6f.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-i2NL9cvj.js";var e=4;function a(o){return r(o,e)}export{a as c};
